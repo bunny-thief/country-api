244b1834d34586a10d08a36990302772fb23d24a
bunny-thief <4421229+bunny-thief@users.noreply.github.com>
2023-02-12 11:47:58 +0100

    Set up mongodb connection

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 require('dotenv').config()
 const PORT = process.env.PORT
 const connectionString = process.env.CONNECTION_STRING
-const db = process.env.db
-const collection = process.env.COLLECTION
+// const db = process.env.db
+// const collection = process.env.COLLECTION
 const express = require('express')
 const app = express()
+const MongoClient = require('mongodb').MongoClient
+
+MongoClient.connect(connectionString)
+    .then(client => {
+        console.log('Connected to Database')
+        const db = client.db(process.env.db)
+        const countries = db.collection(process.env.COLLECTION)
+    })
+    .catch(err => console.error(`$error: {err}, line: ${err.lineNumber} `))
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
