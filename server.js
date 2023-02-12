42317de8ab67f0ac268b5f89c5e5404b6b726a27
bunny-thief <4421229+bunny-thief@users.noreply.github.com>
2023-02-12 11:38:41 +0100

    Set up express

--- server.js
+++ server.js
@@ -3,3 +3,7 @@ const PORT = process.env.PORT
 const connectionString = process.env.CONNECTION_STRING
 const db = process.env.db
 const collection = process.env.COLLECTION
+const express = require('express')
+const app = express()
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
